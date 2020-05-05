02609024d367ceb82c30a63bb9fad7e3b14e632c
pi31415einstein <36282526+pi31415einstein@users.noreply.github.com>
2020-05-05 15:48:26 +1000

    Finished message response and deletion logic

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 # bot.py
-import os
-
-import discord
+import os, discord
 from dotenv import load_dotenv
 
+#Get tokens from .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+#Removes either the username or discord tag.
+detagger = lambda a:str(a)[:-5]
+denamer = lambda a:str(a)[-5:]
+
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Online', end='\n\n')
 
 @client.event
 async def on_typing(channel, user, when):
@@ -16,16 +23,33 @@ async def on_typing(channel, user, when):
     if user == client.user:
         return
 
-    #removes the discord user identifier code and preceding delimiter
-    detagger = lambda a:str(user)[:-5]
-    denamer = lambda a:str(user)[-5:]
+    #Reads the contents of the user lists on startup.
+    f = open('admins.txt', 'r')
+    admins = list(map(lambda a:a.rstrip(),f))   #Iterates through all lines in the file and strips newline chars from the end.
+    f.close()
 
-    if denamer(user) == "#0206":
-        message = 'stop typing'
-        username = detagger(user)
-        await channel.send(f'{user.mention} stop typing')
-        await user.send(message, tts=True)
-    else:
-        await channel.send(f'{user.mention} continue')
+    f = open('discriminatedUsers.txt', 'r')
+    muted = list(map(lambda a:a.rstrip(),f))
+    f.close()
+
+    if denamer(user) in muted:
+        await channel.send(f'{user.mention} please refrain from typing.')
+        await user.send('Please refrain from typing.')
+
+@client.event
+async def on_message(message):
+    if denamer(message.author) != '#3842':
+        await message.delete()
+        try:
+            await delete(message)
+            print(f'\"{message}\" deleted.')
+        except discord.HTTPException:
+            print("HTTP Exception")
+        except discord.Forbidden:
+            print("Forbidden")
+        except discord.NotFound:
+            print("Message not found")
+        except:
+            print('An error occurred during message deletion')
 
 client.run(TOKEN)
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-user = "afa5e#6644"
-detagger = lambda a:user[:-5]
-print(detagger(user))
+f = open('discriminatedUsers.txt', 'r')
+mutedList = list(map(lambda a:a.rstrip(),f))
+f.close()
+
+muted = {}
+for item in mutedList:
+    muted[item] = [0, 0]
+
+print(muted)
