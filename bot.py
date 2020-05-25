92dfd459cf15b6b7e8f2b16536641ae59479776e
pi31415einstein <36282526+pi31415einstein@users.noreply.github.com>
2020-05-25 20:06:50 +1000

    Added a .zip(ped) file for easy install on other devices

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ f = open('discriminatedUsers.txt', 'r')
 muted = list(map(lambda a:a.rstrip(),f))
 f.close()
 
-bot_state = 'on'
+bot_state = True
 
 client = discord.Client()
 
@@ -29,32 +29,35 @@ async def on_ready():
 
 @client.event
 async def on_typing(channel, user, when):
+    global bot_state
     #Check if the user currently typing is the bot to prevent recursion
     if user == client.user:
         return
 
-    if denamer(user) in muted:
-        await channel.send(f'{user.mention} please refrain from typing.')
-        await user.send('Please refrain from typing.')
-        print(f'{user} started typing and was warned', end='\n\n')
+    if bot_state:
+        if denamer(user) in muted:
+            await channel.send(f'{user.mention} please refrain from typing.')
+            await user.send('Please refrain from typing.')
+            print(f'{user} started typing and was warned', end='\n\n')
 
 @client.event
 async def on_message(message):
     global bot_state
+    print(message.content)
     message_author_tag = denamer(message.author)
-    if message_author_tag != '#3842' and bot_state == 'on':      #User #3842 is the bot.
+    if message_author_tag != '#3842' and bot_state:      #User #3842 is the bot.
         if message_author_tag in muted:
             await message.delete()
             print(f'\"{message}\" from {message.author} was deleted.', end='\n\n')
             await message.author.send('Please cease the actions of sending messages')
     elif message_author_tag in admins:
-        print(message.contents, end='\n\n')
-        if '~stop' in message.content:
-            bot_state = 'off'
+        print(message.content, end='\n\n')
+        if message.content.startswith('stop'):
+            bot_state = False
             print(f'Bot has been turned off by {message.author}')
             await message.channel.send(f'Bot has been turned off by {message.author}')
-        elif '~start':
-            bot_state = 'on'
+        elif message.content.startswith('start'):
+            bot_state = True
             print(f'Bot has been turned on by {message.author}')
             await message.channel.send(f'Bot has been turned on by {message.author}')
 client.run(TOKEN)
--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-f = open('discriminatedUsers.txt', 'r')
-mutedList = list(map(lambda a:a.rstrip(),f))
-f.close()
-
-muted = {}
-for item in mutedList:
-    muted[item] = [0, 0]
-
-print(muted)
+import random
+for i = 0 to 100
+    
